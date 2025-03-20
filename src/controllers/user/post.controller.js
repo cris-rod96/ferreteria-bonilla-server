821bd37aa908a7ebf10e8b7bdc839823b2e348a9
Cristhian Rodríguez <crisrodam1996@gmail.com>
2025-03-20 00:46:09 -0500

    Hash password method

--- src/controllers/user/post.controller.js
+++ src/controllers/user/post.controller.js
@@ -1,10 +1,18 @@
 import { request, response } from 'express'
 import { userService } from '../../services/index.services.js'
+import { bcryptUtil } from '../../utils/index.utils.js'
 
 const registerUser = async (req = request, res = response) => {
   try {
     const data = req.body
-    const { code, message } = await userService.registerUser(data)
+    const { password } = data
+
+    const hashPassword = await bcryptUtil.hashPassword(password)
+
+    const { code, message } = await userService.registerUser({
+      ...data,
+      password: hashPassword,
+    })
     res.status(code).json({ message })
   } catch (error) {
     res.status(500).json({
